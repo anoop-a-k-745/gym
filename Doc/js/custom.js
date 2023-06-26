2079c4813dbe0715225a0c9945b5f875f1f16339
unknown <https://github.com/Anoop-A-K/Coursera-Test.git>
2023-06-26 19:47:43 +0530

    first commit

---
+++ Doc/js/custom.js
@@ -0,0 +1,24 @@
+// $(function() {
+// "use strict";
+    
+//     $('#nav').singlePageNav();
+
+// });
+
+// document.getElementById("play-button").addEventListener("click", function() {
+//     var videoPlayer = document.getElementById("video-player");
+//     var playButton = document.getElementById("play-button");
+    
+//     videoPlayer.style.display = "block";
+//     playButton.style.display = "none";
+//   });
+
+
+  document.getElementById("popup-video").addEventListener("click", function() {
+    var videoPlayer = document.getElementById("video-player");
+    var playButton = document.getElementById("popup-video");
+    
+    videoPlayer.style.display = "block";
+    playButton.style.display = "none";
+  });
+  
